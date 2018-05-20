2d160e68c7885bdf3202507918bcc8badc9aa822
Tim Lucas <t@toolmantim.com>
2018-05-20 23:29:15 +1000

    Actually do the thing

--- index.js
+++ index.js
@@ -1,12 +1,64 @@
+const configPath = '.github/homebrew-tap-release.yml'
+
+const compareVersions = require('compare-versions')
+
 module.exports = robot => {
-  robot.on('release.published', async context => {
-    const { payload: { release: { tag_name: tagName, draft, assets } } } = context
+  const run = async context => {
+    const { asset, tap, template } = await context.config('homebrew-tap-release.yml', {})
+    const [ owner, repo, path ] = tap && tap.split('/')
 
-    robot.log(tagName, draft, assets)
-
-    if (draft) {
-      robot.log(`Ignoring draft release`)
+    if (!asset || !owner || !repo || !path || !template) {
+      robot.log(`No valid config found in ${configPath}`)
       return
+    }
+
+    const releases =
+      (await context.github.paginate(
+        context.github.repos.getReleases(context.repo()),
+        res => res.data))
+      .filter(release => !release.draft)
+      .sort((r1, r2) => (compareVersions(r2.tag_name, r1.tag_name)))
+
+    const latestStable = releases.filter(release => !release.prerelease)[0]
+    const latestPrerelease = releases.filter(release => release.prerelease)[0]
+
+    console.log({ latestStable, latestPrerelease })
+
+    // TODO: gsub the following
+    //   $STABLE_URL
+    //   $STABLE_VERSION
+    //   $STABLE_SHA256
+    //   $DEVEL_URL
+    //   $DEVEL_VERSION
+    //   $DEVEL_SHA256
+
+    robot.log(`Updating ${owner}/${repo}/${path}`)
+
+    const existingFile = await context.github.repos.getContent({ owner, repo, path })
+
+    await context.github.repos.updateFile({
+      owner,
+      repo,
+      path,
+      message: `Updated ${path} formula`,
+      content: Buffer.from(template).toString('base64'),
+      sha: existingFile.data.sha
+    })
+
+    robot.log(`Updated ${owner}/${repo}/${path}`)
+  }
+
+  robot.on('release', run)
+
+  robot.on('push', async context => {
+    if (context.payload.commits.some(commit => (
+      commit.added.includes(configPath) ||
+      commit.removed.includes(configPath) ||
+      commit.modified.includes(configPath)
+    ))) {
+      return run(context)
+    } else {
+      robot.log(`Ignoring push that didn't modify ${configPath}`)
     }
   })
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,81 @@
-// You can import your modules
-// const index = require('../index')
+const { createRobot } = require('probot')
+const app = require('../index')
+const payload = require('./fixtures/release')
+const draftPayload = require('./fixtures/release-draft')
 
-test('that we can run tests', () => {
-  // your real tests go here
-  expect(1 + 2 + 3).toBe(6)
+describe('homebrew-tap-release-bot', () => {
+  let robot
+  let github
+
+  beforeEach(() => {
+    robot = createRobot({})
+    app(robot)
+
+    github = {
+      repos: {
+        getContent: jest.fn().mockReturnValueOnce(Promise.resolve({
+          data: {
+            content: Buffer.from(`
+              tap: toolmantim/homebrew-tap-release-bot-test-project-tap/some-tool.rb
+              template: >
+                class TestTool < Formula
+                  homepage "https://github.com/toolmantim/bksr"
+                  url "\${STABLE_URL}"
+                  version "\${STABLE_VERSION}"
+                  sha256 "\${STABLE_SHA256}"
+              
+                  def install
+                    prefix.install "giphy.gif"
+                  end
+                end
+            `).toString('base64')
+          }
+        }))
+      }
+    }
+
+    robot.auth = () => Promise.resolve(github)
+  })
+
+  describe('without a config', () => {
+    beforeEach(() => {
+      const notFoundError = new Error('Not found')
+      notFoundError.code = 404
+      github.repos.getContent = jest.fn().mockReturnValueOnce(Promise.reject(notFoundError))
+    })
+
+    it('does nothing', async () => {
+      await robot.receive({ event: 'release', payload })
+
+      expect(github.repos.getContent).toBeCalledWith({
+        owner: 'toolmantim',
+        repo: 'homebrew-tap-release-bot-test-project',
+        path: '.github/homebrew-tap-release.yml'
+      })
+    })
+  })
+
+  describe('with a draft release', () => {
+    it('ignores it', async () => {
+      await robot.receive({ event: 'release', payload: draftPayload })
+
+      expect(github.repos.getContent).toBeCalledWith({
+        owner: 'toolmantim',
+        repo: 'homebrew-tap-release-bot-test-project',
+        path: '.github/homebrew-tap-release.yml'
+      })
+    })
+  })
+
+  describe('with a config', () => {
+    it('does something', async () => {
+      await robot.receive({ event: 'release', payload })
+
+      expect(github.repos.getContent).toBeCalledWith({
+        owner: 'toolmantim',
+        repo: 'homebrew-tap-release-bot-test-project',
+        path: '.github/homebrew-tap-release.yml'
+      })
+    })
+  })
 })
-
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
